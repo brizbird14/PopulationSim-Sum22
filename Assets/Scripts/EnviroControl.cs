8ae9f02bf06ce3fac9e6d387dd61f719a0b59001
brizbird14 <brizbird14@gmail.com>
2022-05-27 09:59:24 -0400

    Early design
    Early dev - basic enviro controls and importing assets

---
+++ Assets/Scripts/EnviroControl.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnviroControl : MonoBehaviour
+{
+    public void Start() {
+        // Set starting light intensity / brightness
+        lightSlider.value = 0.5f;
+        sunlight.intensity = lightSlider.value + 0.25f;
+
+        // Set pond activity
+        pond.SetActive(false);
+        pondOn = false;
+    }
+
+    // Controlling local light intensity
+    [SerializeField] Light sunlight;
+    [SerializeField] Slider lightSlider;
+    public void UpdateLight() {
+        sunlight.intensity = lightSlider.value + 0.25f;
+    }
+
+    // Controlling pond appearance
+    [SerializeField] GameObject pond;
+    bool pondOn;
+    public void TogglePond() {
+        if (pondOn) {
+            // Turn pond off
+            pond.SetActive(false);
+            pondOn = false;
+        } else {
+            // Turn pond on
+            pond.SetActive(true);
+            pondOn = true;
+        }
+    }
+}
