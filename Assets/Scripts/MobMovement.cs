661c62679d9a98c051f57acee0f84c8c50762d66
brizbird14 <brizbird14@gmail.com>
2022-06-08 16:52:24 -0400

    Unity version and movement
    Move from Unity 2020.3.32f1 to 2021.3.4f1, movement script

---
+++ Assets/Scripts/MobMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobMovement : MonoBehaviour
+{
+
+    [SerializeField] float mobSpeed; // Speed multiplier
+    bool turnStop; // When true, suspends movement while turning
+    float timeToTurn; // When next change of direction
+    float moveDirec; // Randomizes movement direction
+
+    void Start() {
+        changeDirection();
+        timeToTurn = Time.time + Random.Range(1.5f, 5.0f);
+        turnStop = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= timeToTurn) {
+            changeDirection();
+            timeToTurn = Time.time + Random.Range(1.5f, 5.0f);
+        }
+
+        if (turnStop) {
+            Debug.Log("Stopping");
+            this.gameObject.transform.position = this.gameObject.transform.position;
+        } else if (moveDirec < 1.0f) {
+            this.gameObject.transform.position += this.gameObject.transform.up * Time.deltaTime * mobSpeed;
+        } else if (moveDirec < 2.0f) {
+            this.gameObject.transform.position -= this.gameObject.transform.up * Time.deltaTime * mobSpeed;
+        } else if (moveDirec < 3.0f) {
+            this.gameObject.transform.position += this.gameObject.transform.right * Time.deltaTime * mobSpeed;
+        } else {
+            this.gameObject.transform.position -= this.gameObject.transform.right * Time.deltaTime * mobSpeed;
+        }
+    }
+
+    void changeDirection () {
+        Debug.Log("Changing direction");
+        moveDirec = Random.Range(0.0f, 4.0f);
+    }
+
+    void OnCollisionEnter (Collision col) {
+        // Upon hitting walls
+        turnStop = true;
+        if (col.gameObject.tag == "Environment") {
+            Debug.Log("Hit wall");
+            // Rotate away from wall
+            this.gameObject.transform.Rotate(0.0f, Random.Range(-100.0f, 100.0f), 0.0f, Space.Self);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnControl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnControl : MonoBehaviour
+{
+    GameObject mobShroom;
+
+    // Spawn rates, will b controlled by script eventually
+
+    // Spawn locations, manual input in editor
+    [SerializeField] float shroomX, shroomZ;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
